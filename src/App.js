1c9fded396bc3b89081d888bfb66694e01c3b3b3
shirong <sykim2@parkingcloud.co.kr>
2018-09-13 18:51:41 +0900

    메인 페이지 작업 완료

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import { HashRouter as Router, Route} from 'react-router-dom';
+import { ApolloProvider } from 'react-apollo';
+import client from './apollo';
+
+import Home from './Home';
+import Detail from './Detail';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+      <ApolloProvider client={client}>
+        <Router>
+          <React.Fragment>
+            <Route exact={true} path={'/'} component={Home} />
+            <Route path={'/details/:movieId'} component={Detail} />
+          </React.Fragment>
+        </Router>
+      </ApolloProvider>
     );
   }
 }
